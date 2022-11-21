510eab36d96dd629ea4593757dba0c0d2a691734
malikShort <93655412+malikShort@users.noreply.github.com>
2022-11-21 08:37:12 -0500

    Add files via upload

---
+++ Veiws/aditionalInfo.view.php
@@ -0,0 +1,40 @@
+
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="css/main-display.css" type="text/css">
+
+</head>
+<body>
+
+
+<div class="row">
+<p>patient ID</p>  <input type="text">
+    </div>
+
+    <div class="row">
+<p>Group</p>  <input type="text">
+    </div>
+
+    <div class="row">
+<p>Admisson Data</p>  <input type="text">
+    </div>
+
+
+
+
+    <div class="row">
+<p>patient name</p>  <input type="text">
+    </div>
+
+
+    <div class="row">
+<button> ok </button>
+
+<button> cancel </button>
+</div>
+
+
+    
+</body>
+</html>
---
+++ Veiws/employee.view.php
@@ -0,0 +1,21 @@
+<html>
+
+<P>Id</P> <P></P>
+
+<P>Name</P> <P></P>
+
+<P>Role</P> <P></P>
+
+<P>Salary</P> <P></P>
+
+<P>Emp ID</P> <input type="text">
+
+<P>New Salary</P> <input type="text">
+
+<button> ok </button>
+
+<button> Cancel </button>
+
+
+
+</html>
