9225eadc690263c71fd1bf6b4472f33961f663e1
Gabe-Dombach <gdombach009@gmail.com>
2022-11-21 13:37:52 -0500

    Added DOM based Role Selection options to the register page

--- Scripts/register.php
+++ Scripts/register.php
@@ -1,5 +1,11 @@
 <?php 
+session_start();
+require "dbFunctions.php";
+$db = dbConnect($host, $port, $dbname, $credentials);
 
+if (isset($_POST['submit'])){
 
+}
 
+require "../Veiws/register.veiw.php"
 ?>
