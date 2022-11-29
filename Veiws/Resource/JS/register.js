2852050acbdbb9fbb66b83f26c64db034f28f6fc
Gabe-Dombach <gdombach009@gmail.com>
2022-11-29 18:48:30 -0500

    a

--- Veiws/Resource/JS/register.js
+++ Veiws/Resource/JS/register.js
@@ -1,13 +1,14 @@
-
-$("#roles").change(function () {
+$(document).ready(function () {
+    $('.patientOnly').css({ visibility: 'hidden' });
+});
+$('#roles').change(function () {
     let role = $(this).children('option').filter(':selected').text();
     if (role == 'Patient') {
-        console.log("visible");
-            $('.patientOnly').css({ visibility: 'visible' });
-        }
-    else {
-                console.log('invisible');
+        console.log('visible');
+        $('.patientOnly').css({ visibility: 'visible' });
+    } else {
+        console.log('invisible');
 
-            $('.patientOnly').css({ visibility: 'hidden' });
-        }
-    });
+        $('.patientOnly').css({ visibility: 'hidden' });
+    }
+});
