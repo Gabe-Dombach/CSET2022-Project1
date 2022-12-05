a2811ef0fd05c5b34ef99ddeeae1132c86062c04
Gabe-Dombach <gdombach009@gmail.com>
2022-12-05 09:45:04 -0500

    Roles.php is finished

---
+++ Scripts/role.php
@@ -0,0 +1,40 @@
+<?php 
+session_start();
+require "dbFunctions.php";
+$db = dbConnect($host, $port, $dbname, $credentials);
+
+if (!isset($_SESSION['user']) || $_SESSION['level'] != '4') {
+    if (isset($_SESSION['user'])) {
+        unset($_SESSION['user']);
+        if (isset($_SESSION['role'])) {
+            unset($_SESSION['role']);
+        }
+    }
+    header("Location:login.php?error=Administrator privlages required to acess roles!");
+}
+
+if(isset($_POST['submit'])){
+    $role = $_POST['roleName'];
+    $level = intval($_POST['access_level']);
+    $sql = "SELECT * FROM roles WHERE roles = '$role';";
+    $ret = pg_query($db, $sql);
+    $check = pg_fetch_all($ret);
+    if(!$check){
+
+    $sql = "INSERT INTO roles
+            VALUES
+            ('$role',$level);";
+
+    $ret = pg_query($db, $sql);
+    if(!$ret){
+        echo pg_last_error($db);
+        exit();
+    }
+    echo "Role: $role added successfully";
+}
+else{
+    echo "Error: Role: $role already exists";
+}
+}
+require("../Veiws/role.view.php");
+?>
