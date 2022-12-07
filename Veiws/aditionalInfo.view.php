797d4dc104df3889387c2cb9ae9e1dd99f0b0ac8
juryjaben <juryjaben@gmail.com>
2022-12-07 09:21:28 -0500

    Completed Employee, Doctor Appointment, Working on Prescription

--- Veiws/aditionalInfo.view.php
+++ Veiws/aditionalInfo.view.php
@@ -1,4 +1,3 @@
-
 <html lang="en">
 <head>
 
@@ -7,34 +6,48 @@
 </head>
 <body>
 
-
+<form action="../Scripts/aditionalInfo.php" method="POST">
 <div class="row">
-<p>patient ID</p>  <input type="text">
+<p>patient ID</p>  <input type="text" name="patientId" id="patientId" onkeyup="updateIdField()">
     </div>
 
     <div class="row">
-<p>Group</p>  <input type="text">
+<p>Group</p>  <input type="type" name="group" id="group">
     </div>
 
     <div class="row">
-<p>Admisson Data</p>  <input type="text">
+<p> Admission Date </p>  <input type="date" name="admissionDate" id="admissionDate">
     </div>
 
-
-
-
     <div class="row">
-<p>patient name</p>  <input type="text">
+<p>patient name</p>  <input type="text" id="patientName" readonly>
     </div>
 
-
     <div class="row">
-<button> ok </button>
+    <input type="submit" name="submit" value="ok">
 
-<button> cancel </button>
+    <button onclick="clearFields()"> cancel </button>
 </div>
+</form> 
 
-
+<script>
+    let idInput = document.getElementById("patientId");
+    let groupInput = document.getElementById("group");
+    let patientName = document.getElementById("patientName");
+    let dateInput = document.getElementById("admissionDate");
+    let patients= <?php echo json_encode($patients); ?>;
+    function updateIdField() {
+        let currPatient = patients.filter( i => i["patientid"] == idInput.value );
+        let currPatientName = ""+currPatient[0]["lname"]+", "+currPatient[0]["fname"];
+        patientName.value = currPatientName;
+    }
+    function clearFields() {
+        idInput.value = "";
+        dateInput.value = "";
+        patientName.value = "";
+        groupInput.innerHTML = "";
+    }
+</script>
     
 </body>
 </html>
