417066d3a1c330a95f83338abe50d8c9f0a7a2d2
Gabe-Dombach <gdombach009@gmail.com>
2022-12-13 09:26:49 -0500

    converted Roster to a table setup

--- Veiws/roster.view.php
+++ Veiws/roster.view.php
@@ -2,21 +2,22 @@
 <html lang="en">
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="../Veiws/css/main-display.css" type="text/css">
-    <style>
+        <style>
         table, th, td {
             border:1px solid black;
             border-collapse:collapse;
-            width: 50vw;
+            width: 60vw;
 
             padding:2vh;
             text-align:center;
 
         }
         table{
-            margin:5vw;
+            margin:2vw;
         }
         </style>
+    <link rel="stylesheet" href="../Veiws/css/main-display.css" type="text/css">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -33,12 +34,10 @@
    
 
     <form action="../Scripts/roster.php" method="post">
-    <p> date </p><input id="date" type="date" name="date" value=<?php echo date('Y-m-d'); ?>>
-    <input type="submit" value="Check_Date" name="submit" class="buttonS">
-    </form>
     <div>
         <table>
             <tr>
+                <th>Date</th>
                 <th>Supervisor</th>
                 <th>Doctor</th>
                 <th>Caregiver 1</th>
@@ -47,6 +46,9 @@
                 <th>Caregiver 4</th>
             </tr>
             <tr>
+                <td>
+                    <input id="date" type="date" name="date" value=<?php echo date('Y-m-d'); ?>>
+                </td>
                 <td>
                     <?php
                         if (isset($cg1)) {
@@ -133,7 +135,9 @@
         </td>
             </tr>
         </table>
+            <input type="submit" value="Check_Date" name="submit" class="buttonS">
 
+</form>
         <?php if(isset($_GET['error'])){echo $_GET['error'];}
                 else{echo '';} ?>
     </div>
